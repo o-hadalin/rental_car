15200427ee25949900a4ef27bbcdf986140f7abd
O. HADALIN <dyblikat777@gmail.com>
2025-05-18 21:50:23 +0300

    fix: title wrapping in CarCard to keep uniform card height; info about no results filter

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -5,6 +5,15 @@ import styles from './CarList.module.css';
 
 const CarList = () => {
   const cars = useSelector(selectCars);
+
+  if (cars.length === 0) {
+    return (
+      <p className={styles.emptyMessage}>
+        Nothing found. Please change your search parameters.{' '}
+      </p>
+    );
+  }
+
   return (
     <ul className={styles.list}>
       {cars.map(car => (
