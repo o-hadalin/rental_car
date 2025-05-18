66b6c8a90d9c46f7827d0d7aeb6add4086d5f6b3
O. HADALIN <dyblikat777@gmail.com>
2025-05-19 00:01:35 +0300

    dell info about nothing found from filter after reload page

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,15 +1,22 @@
 import { useSelector } from 'react-redux';
-import { selectCars } from '../../redux/cars/selectors';
+import { selectCars, selectIsLoading } from '../../redux/cars/selectors.js';
+import { selectIsFiltered } from '../../redux/filters/selectors.js';
 import CarCard from '../CarCard/CarCard';
 import styles from './CarList.module.css';
 
 const CarList = () => {
   const cars = useSelector(selectCars);
+  const isLoading = useSelector(selectIsLoading);
+  const isFiltered = useSelector(selectIsFiltered);
 
-  if (cars.length === 0) {
+  if (isLoading) {
+    return <div className={styles.spinner} />;
+  }
+
+  if (cars.length === 0 && isFiltered) {
     return (
       <p className={styles.emptyMessage}>
-        Nothing found. Please change your search parameters.{' '}
+        Nothing found. Please change your search parameters.
       </p>
     );
   }
--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -3,3 +3,13 @@ export const selectPrice = state => state.filters.price;
 export const selectMileageFrom = state => state.filters.mileageFrom;
 export const selectMileageTo = state => state.filters.mileageTo;
 export const selectLastSearch = state => state.filters.lastSearch;
+
+export const selectIsFiltered = state => {
+  const { brand, price, mileageFrom, mileageTo } = state.filters;
+  return (
+    (brand && brand.length > 0) ||
+    (price && price.length > 0) ||
+    (mileageFrom !== null && mileageFrom !== undefined && mileageFrom !== '') ||
+    (mileageTo !== null && mileageTo !== undefined && mileageTo !== '')
+  );
+};
